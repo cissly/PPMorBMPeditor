3d49722350259974b62c359c3f77a028147afea7
cissly <tkdwls237@naver.com>
2023-11-11 14:15:03 +0900

    restruct

--- src/Doc.java
+++ src/Doc.java
@@ -3,22 +3,28 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Doc {
 
     String filePath;
-    public Doc()
+    public static void main(String[] args)
     {
+        JFrame ff = new JFrame("aaa");
+        ff.setContentPane(new View().main);
+        ff.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ff.setSize(500,500);
+        ff.setVisible(true);
     }
 
     public boolean Check(String target) // 파일이 유코드인지 확인후 맞다면 true를 반환한다.
     {
-
         if((target.length()-4)==target.indexOf(".uco")) return true;
         else return false;
     }
 
-    public String[] lineReader(String path)  {
+    public ArrayList<String> lineReader(String path)  { // 라인을 한개씩 읽어 Arraylist에 넣는 작업
+        ArrayList<String> result = new ArrayList<>();
         if(!Check(path))
         {
             return null;
@@ -26,7 +32,17 @@ public class Doc {
         try
         {
             BufferedReader br = new BufferedReader(new FileReader(path));
+            String input = br.readLine();
+            while (input != null) {
+                // do something...
+                result.add(input);
+                input = br.readLine();
+            }
         }
+        catch(FileNotFoundException e) {}
+        catch(IOException e) {}
+
+        return result;
     }
 
 
--- src/View.java
+++ src/View.java
@@ -3,20 +3,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class View {
-    private JButton button1;
-    private JPanel main;
+    private JButton INPUT;
+    public JPanel main;
+    private JButton NEXT;
+    private JScrollPane UCODE;
+    private JScrollPane STACK;
+    private JScrollPane RESULT;
+    private JButton OUTPUT;
 
-    public static void main(String[] args)
-    {
-        JFrame ff = new JFrame("aaa");
-        ff.setContentPane(new View().main);
-        ff.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ff.setSize(500,500);
-        ff.setVisible(true);
-    }
 
     public View() {
-        button1.addActionListener(new ActionListener() {
+        new JScrollPane(UCODE, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        UCODE.add(new JLabel("zz"));
+        INPUT.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(null,"hello world");
